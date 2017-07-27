e6b247e8fd9c1d7fb6dbe1a13900288e398a83a7
Terry Bui <terry.bui96@gmail.com>
2017-07-27 15:08:51 +1000

    trim module down to new specification, added search by user

--- postproc.sh
+++ postproc.sh
@@ -6,4 +6,55 @@ string="<input ref=\"Table\">"
 replacement="<input ref=\"Table\" faims_table=\"true\">"
 perl -0777 -i.original -pe "s/\\Q$string/$replacement/igs" ui_schema.xml
 
+string="
+        <select1 ref=\"Entity_Types\">
+          <label>{Entity_Types}<\/label>
+          <item>
+            <label>Options not loaded<\/label>
+            <value>Options not loaded<\/value>
+          <\/item>
+        <\/select1>"
+replacement="
+        <group ref=\"Colgroup_1\" faims_style=\"orientation\">
+          <label\/>
+          <group ref=\"Col_0\" faims_style=\"even\">
+            <label\/>
+            <select1 ref=\"Entity_Types\">
+              <label>{Entity_Types}<\/label>
+              <item>
+                <label>Options not loaded<\/label>
+                <value>Options not loaded<\/value>
+              <\/item>
+            <\/select1>
+          <\/group>
+          <group ref=\"Col_1\" faims_style=\"even\">
+            <label\/>
+            <select1 ref=\"Select_Author\">
+              <label>{Select_Author}<\/label>
+              <item>
+                <label>Options not loaded<\/label>
+                <value>Options not loaded<\/value>
+              <\/item>
+            <\/select1>
+          <\/group>
+        <\/group>"
+perl -0777 -i.original -pe "s/$string/$replacement/igs" ui_schema.xml
+
+string="
+              <Entity_Types\/>"
+replacement="
+              <Colgroup_1>
+                <Col_0>
+                  <Entity_Types\/>
+                <\/Col_0>
+                <Col_1>
+                  <Select_Author\/>
+                <\/Col_1>
+              <\/Colgroup_1>"
+perl -0777 -i.original -pe "s/$string/$replacement/igs" ui_schema.xml
+
+cat << EOF >> english.0.properties
+Select_Author=Select Author
+EOF
+
 rm ui_schema.xml.original
